96f73cdab271253731139604f4d00f5c94679dc2
“sd63534790 <44766986@qq.com>
2016-12-07 00:02:00 +0800

    1
    1

---
+++ app/src/main/java/cheny/com/gank/todolist/task/Activity_TaskList.java
@@ -0,0 +1,31 @@
+package cheny.com.gank.todolist.task;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import cheny.com.gank.todolist.R;
+
+/**
+ * 作者： by y on 2016/12/5.
+ * QQ：  44766986
+ * 邮箱：44766986@qq.com
+ */
+
+public class Activity_TaskList extends AppCompatActivity {
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tasklist);
+    }
+}
